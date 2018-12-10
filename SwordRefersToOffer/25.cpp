1ec9229f9a25d4d090319084a68c6c51a365c0a3
lzj112 <872575628@qq.com>
2018-12-10 14:54:29 +0800

    合并两个有序链表

---
+++ SwordRefersToOffer/25.cpp
@@ -0,0 +1,58 @@
+/*
+合并了;两个排序的链表
+*/
+
+#include <iostream>
+
+
+struct ListNode
+{
+	int val;
+	struct ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution 
+{
+public:
+    ListNode* Merge(ListNode* pHead1, ListNode* pHead2)
+    {
+        if (pHead1 == nullptr)
+        {
+            return pHead2;
+        }
+        else if (pHead2 == nullptr) 
+        {
+            return pHead1;
+        }
+
+        ListNode* p1 = pHead1, * p2 = pHead2;
+        ListNode* p0 = new ListNode(1);
+        ListNode* head = p0;
+
+        while (p1 != nullptr && p2 != nullptr) 
+        {
+            if (p1->val > p2->val) 
+            {
+                p0->next = p2;
+                p0 = p0->next;
+                p2 = p2->next;
+            }
+            else 
+            {
+                p0->next = p1;
+                p0 = p0->next;
+                p1 = p1->next;
+            }
+        }
+        if (p1 != nullptr) 
+        {
+            p0->next = p1;
+        }
+        if (p2 != nullptr) 
+        {
+            p0->next = p2;
+        }
+        return head->next;
+    }
+};
